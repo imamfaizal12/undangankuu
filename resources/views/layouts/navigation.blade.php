f987ec4ecfc9cdad61849a66580a03929f17e938
Imam Faizal <imamfreecs@gmail.com>
2020-09-02 18:36:07 +0700

    ubah nav-item jadi active

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -6,16 +6,16 @@
 
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
+      <li class="nav-item{{ request()->is('/') ? ' active' : '' }}">
         <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
       </li>
-      <li class="nav-item">
+      <li class="nav-item{{ request()->is('about') ? ' active' : '' }}">
         <a class="nav-link" href="/about">About</a>
       </li>
-      <li class="nav-item">
+      <li class="nav-item{{ request()->is('contact') ? ' active' : '' }}">
         <a class="nav-link" href="/contact">Contact</a>
       </li>
-      <li class="nav-item">
+      <li class="nav-item{{ request()->is('login') ? ' active' : '' }}">
         <a class="nav-link" href="/login">Login</a>
       </li>
   </div>
